faed67f9177b27142dfce8066d2f8e49ecd93c67
Mackenzie Ortiz <mlubben1@gmail.com>
2023-10-22 23:56:21 -0400

    Add files via upload

---
+++ week-1/week-1/books.js
@@ -0,0 +1,30 @@
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+
+class BookStore {
+    books = [];
+
+    constructor() {
+        this.books = [
+            new Book('The Hobbit', 'J.R.R. Tolkien'),
+            new Book('The Fellowship of the Ring', 'J.R.R. Tolkien'),
+            new Book('The Two Towers', 'J.R.R. Tolkien'),
+            new Book('The Return of the King', 'J.R.R. Tolkien'),
+            new Book('Dune', 'Frank Herbert')
+        ]
+    }
+
+    showBooks() {
+        console.log('\n  -- DISPLAYING AVAILABLE BOOKS --');
+        for (let book of this.books) {
+            let currentBook = book;
+        }
+    }
+}
+
+let bookStore = new BookStore();
+bookStore.showBooks(); 
